b408109ed4ee63ce49d0d1d50569052fe264052b
purpleclay <purpleclaygh@gmail.com>
2024-07-13 05:56:10 +0100

    predicates with range support

--- predicate_test.go
+++ predicate_test.go
@@ -43,6 +43,76 @@ func TestWhile(t *testing.T) {
 	}
 }
 
+func TestWhileN(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		input string
+		rem   string
+		ext   string
+	}{
+		{
+			name:  "Ascii",
+			input: "2024 adventure awaits",
+			rem:   " adventure awaits",
+			ext:   "2024",
+		},
+		{
+			name:  "Unicode",
+			input: "2024 年の冒険が待っています",
+			rem:   " 年の冒険が待っています",
+			ext:   "2024",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			rem, ext, err := chomp.WhileN(chomp.IsDigit, 2)(tt.input)
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.rem, rem)
+			assert.Equal(t, tt.ext, ext)
+		})
+	}
+}
+
+func TestWhileNM(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		input string
+		rem   string
+		ext   string
+	}{
+		{
+			name:  "Ascii",
+			input: "2024 adventure awaits",
+			rem:   " adventure awaits",
+			ext:   "2024",
+		},
+		{
+			name:  "Unicode",
+			input: "2024 年の冒険が待っています",
+			rem:   " 年の冒険が待っています",
+			ext:   "2024",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			rem, ext, err := chomp.WhileNM(chomp.IsDigit, 1, 8)(tt.input)
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.rem, rem)
+			assert.Equal(t, tt.ext, ext)
+		})
+	}
+}
+
 func TestWhileNot(t *testing.T) {
 	t.Parallel()
 
@@ -70,6 +140,76 @@ func TestWhileNot(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			rem, ext, err := chomp.WhileNot(chomp.IsDigit)(tt.input)
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.rem, rem)
+			assert.Equal(t, tt.ext, ext)
+		})
+	}
+}
+
+func TestWhileNotN(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		input string
+		rem   string
+		ext   string
+	}{
+		{
+			name:  "Ascii",
+			input: "2024 adventure awaits",
+			rem:   "adventure awaits",
+			ext:   "2024 ",
+		},
+		{
+			name:  "Unicode",
+			input: "2024 年の冒険が待っています",
+			rem:   "年の冒険が待っています",
+			ext:   "2024 ",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			rem, ext, err := chomp.WhileNotN(chomp.IsLetter, 2)(tt.input)
+
+			require.NoError(t, err)
+			assert.Equal(t, tt.rem, rem)
+			assert.Equal(t, tt.ext, ext)
+		})
+	}
+}
+
+func TestWhileNotNM(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name  string
+		input string
+		rem   string
+		ext   string
+	}{
+		{
+			name:  "Ascii",
+			input: "2024 adventure awaits",
+			rem:   "adventure awaits",
+			ext:   "2024 ",
+		},
+		{
+			name:  "Unicode",
+			input: "2024 年の冒険が待っています",
+			rem:   "年の冒険が待っています",
+			ext:   "2024 ",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			rem, ext, err := chomp.WhileNotNM(chomp.IsLetter, 1, 8)(tt.input)
 
 			require.NoError(t, err)
 			assert.Equal(t, tt.rem, rem)
