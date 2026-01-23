df019d861baa10d14c516e889f8c94e4c95a9004
Purple Clay <purpleclaygh@gmail.com>
2026-01-23 06:11:59 +0000

    chore(nix): migrate over to go-overlay for improved go version management (#74)
    Signed-off-by: purpleclay <purpleclaygh@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,22 +1,26 @@
 {
-  description = "Makes chomping bytes easy in Go";
+  description = "A parser combinator library for chomping strings (a rune at a time) in Go";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    go-overlay.url = "github:purpleclay/go-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, go-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ go-overlay.overlays.default ];
+        };
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
             git
-            go
+            (go-bin.fromGoMod "${self}/go.mod")
             gofumpt
             golangci-lint
             go-task
