f40882cd35d978a7bca92550c9e27af43f2d3a12
purpleclay <purpleclaygh@gmail.com>
2026-01-23 06:38:26 +0000

    chore(nix): migrate all existing github workflows to use nix and enable git hooks
    Closes #75
    
    Signed-off-by: purpleclay <purpleclaygh@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,28 +4,69 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    go-overlay.url = "github:purpleclay/go-overlay";
+
+    go-overlay = {
+      url = "github:purpleclay/go-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, go-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    go-overlay,
+    git-hooks,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ go-overlay.overlays.default ];
+          overlays = [go-overlay.overlays.default];
+        };
+
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          package = pkgs.prek;
+          hooks = {
+            alejandra = {
+              enable = true;
+              settings = {
+                check = true;
+              };
+            };
+
+            typos = {
+              enable = true;
+              entry = "${pkgs.typos}/bin/typos";
+            };
+          };
         };
       in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            git
-            (go-bin.fromGoMod "${self}/go.mod")
-            gofumpt
-            golangci-lint
-            go-task
-          ];
-        };
-      }
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit (pre-commit-check) shellHook;
+
+            buildInputs =
+              [
+                alejandra
+                (go-bin.fromGoMod "${self}/go.mod")
+                gofumpt
+                golangci-lint
+                nil
+                typos
+              ]
+              ++ pre-commit-check.enabledPackages;
+          };
+        }
     );
 }
